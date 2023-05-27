fbcb96dcb0e9f19780844fbceadfa6c652ef18dc
Jose Almir <joseamfj@outlook.com>
2023-05-27 17:01:52 -0300

    Improve test impl in static array test

--- Arrays/Static_Arrays/test/static_array_test.cpp
+++ Arrays/Static_Arrays/test/static_array_test.cpp
@@ -1,54 +1,79 @@
 #include <gtest/gtest.h>
 #include "../src/static_array.cpp"  // Include the source file to test
-#include <iterator> 
+#include <iterator>
 
-TEST(StaticArrayTest, BasicInitializationAndAccess) {
-    StaticArray<int, 5> arr;
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 3;
-    arr[3] = 4;
-    arr[4] = 5;
-
-    for (int i = 0; i < arr.size(); ++i) {
-        EXPECT_EQ(arr[i], (i + 1));
+// Test case for basic initialization and access of StaticArray
+TEST(StaticArrayTestSuite, TestBasicInitializationAndAccess) {
+    StaticArray<int, 5> testArray;
+    for(int i = 0; i < testArray.size(); ++i) {
+        testArray[i] = i + 1;
+        EXPECT_EQ(testArray[i], i + 1);
     }
 }
 
-TEST(StaticArrayTest, ModifyingElements) {
-    StaticArray<int, 3> arr;
-    arr[0] = 10;
-    arr[1] = 20;
-    arr[2] = 30;
-
-    arr[1] = 25;
-    EXPECT_EQ(arr[1], 25);
-
-    arr[2] += 5;
-    EXPECT_EQ(arr[2], 35);
+// Test case for default initialization of StaticArray
+TEST(StaticArrayTestSuite, TestDefaultInitialization) {
+    StaticArray<int, 5> testArray;
+    for(int i = 0; i < testArray.size(); ++i) {
+        EXPECT_EQ(testArray[i], 0);
+    }
 }
 
-TEST(StaticArrayTest, OutOfBoundsAccess) {
-    StaticArray<int, 5> arr;
+// Test case for modifying elements of StaticArray
+TEST(StaticArrayTestSuite, TestModifyingElements) {
+    StaticArray<int, 3> testArray = {10, 20, 30};
 
-    EXPECT_THROW(arr[5] = 10, std::out_of_range);
+    testArray[1] = 25;
+    EXPECT_EQ(testArray[1], 25);
+
+    testArray[2] += 5;
+    EXPECT_EQ(testArray[2], 35);
 }
 
+// Test case for copy constructor and assignment operator
+TEST(StaticArrayTestSuite, TestCopyConstructorAndAssignment) {
+    StaticArray<int, 3> originalArray = {10, 20, 30};
+    StaticArray<int, 3> copiedArray(originalArray);  // Copy construction
+    StaticArray<int, 3> assignedArray;
+    assignedArray = originalArray;  // Copy assignment
 
-TEST(StaticArrayTest, CustomObject) {
+    for(int i = 0; i < originalArray.size(); ++i) {
+        EXPECT_EQ(copiedArray[i], originalArray[i]);
+        EXPECT_EQ(assignedArray[i], originalArray[i]);
+    }
+}
+
+// Test case for access beyond the StaticArray boundary
+TEST(StaticArrayTestSuite, TestOutOfBoundsAccess) {
+    StaticArray<int, 5> testArray;
+
+    EXPECT_THROW(testArray[5] = 10, std::out_of_range);
+}
+
+// Test case for iterator functionality of StaticArray
+TEST(StaticArrayTestSuite, TestIteratorFunctionality) {
+    StaticArray<int, 5> testArray = {1, 2, 3, 4, 5};
+    int value = 1;
+
+    for(auto it = testArray.begin(); it != testArray.end(); ++it) {
+        EXPECT_EQ(*it, value);
+        ++value;
+    }
+}
+
+// Test case for StaticArray containing custom objects
+TEST(StaticArrayTestSuite, TestCustomObject) {
     struct Person {
         std::string name;
         int age;
     };
 
-    StaticArray<Person, 2> arr;
-    arr[0] = {"Alice", 25};
-    arr[1] = {"Bob", 30};
+    StaticArray<Person, 2> testArray = {{"Alice", 25}, {"Bob", 30}};
 
-    EXPECT_EQ(arr[0].name, "Alice");
-    EXPECT_EQ(arr[0].age, 25);
-    EXPECT_EQ(arr[1].name, "Bob");
-    EXPECT_EQ(arr[1].age, 30);
+    EXPECT_EQ(testArray[0].name, "Alice");
+    EXPECT_EQ(testArray[0].age, 25);
+    EXPECT_EQ(testArray[1].name, "Bob");
+    EXPECT_EQ(testArray[1].age, 30);
 }
 
 int main(int argc, char** argv) {
